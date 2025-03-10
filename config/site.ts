f74e024d0defa2ef0578ae0eefe21275f962ddc1
Leonardo Bazán <leonardo.bazan@daduva.mx>
2025-03-09 23:17:21 -0600

    Fix Links for social media

--- config/site.ts
+++ config/site.ts
@@ -37,8 +37,8 @@ export const siteConfig = {
   ],
   links: {
     instagram: "https://www.instagram.com/paul.t30",
-    facebook: "https://open.spotify.com/intl-es/artist/56CzSMvDKHkeNOZqtlqq3o?si=6TrMbbKjQ-ul2AqhSSiRgw",
-    twitter: "https://music.apple.com/mx/artist/pablo-marte/1494510551",
-    tiktok: "https://music.amazon.com.mx/artists/B083WNNZ92/pablo-marte?marketplaceId=ART4WZ8MWBX2Y&musicTerritory=MX&ref=dm_sh_Ze0NahsGExBAGRfVuH0kobKle"
+    facebook: "https://www.facebook.com/Pablomartemx",
+    twitter: "https://x.com/pablomarte37",
+    tiktok: "https://www.tiktok.com/@pablomarte27"
   }
 };
